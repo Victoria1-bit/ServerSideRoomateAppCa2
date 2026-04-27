25a2d7decfb56c874f112a8c705fe6135e3f97ae
Chibuikem Mathew Uche <chibuikemu13@gmail.com>
2026-04-27 22:35:48 +0100

    bug fixes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
-﻿<?php
+<?php
  
+use App\Http\Controllers\Auth\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
@@ -16,9 +17,9 @@ Route::post('/guest-login', function () {
     $guest = User::firstOrCreate(
         ['email' => 'guest@roommate.local'],
         [
-            'name'     => 'Guest User',
+            'name' => 'Guest User',
             'password' => bcrypt('guest12345'),
-            'role'     => 'member',
+            'role' => 'member',
         ]
     );
  
@@ -30,24 +31,29 @@ Route::post('/guest-login', function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
  
-    Route::get('/chores',                    [ChoreController::class, 'index'])->name('chores.index');
-    Route::get('/chores/create',             [ChoreController::class, 'create'])->name('chores.create');
-    Route::post('/chores',                   [ChoreController::class, 'store'])->name('chores.store');
-    Route::get('/chores/{chore}/edit',       [ChoreController::class, 'edit'])->name('chores.edit');
-    Route::put('/chores/{chore}',            [ChoreController::class, 'update'])->name('chores.update');
+    Route::get('/chores', [ChoreController::class, 'index'])->name('chores.index');
+    Route::get('/chores/create', [ChoreController::class, 'create'])->name('chores.create');
+    Route::post('/chores', [ChoreController::class, 'store'])->name('chores.store');
+    Route::get('/chores/{chore}/edit', [ChoreController::class, 'edit'])->name('chores.edit');
+    Route::put('/chores/{chore}', [ChoreController::class, 'update'])->name('chores.update');
     Route::patch('/chores/{chore}/complete', [ChoreController::class, 'complete'])->name('chores.complete');
-    Route::delete('/chores/{chore}',         [ChoreController::class, 'destroy'])->name('chores.destroy');
+    Route::delete('/chores/{chore}', [ChoreController::class, 'destroy'])->name('chores.destroy');
  
-    Route::get('/expenses',                  [ExpenseController::class, 'index'])->name('expenses.index');
-    Route::get('/expenses/create',           [ExpenseController::class, 'create'])->name('expenses.create');
-    Route::post('/expenses',                 [ExpenseController::class, 'store'])->name('expenses.store');
-    Route::get('/expenses/{expense}/edit',   [ExpenseController::class, 'edit'])->name('expenses.edit');
-    Route::put('/expenses/{expense}',        [ExpenseController::class, 'update'])->name('expenses.update');
-    Route::delete('/expenses/{expense}',     [ExpenseController::class, 'destroy'])->name('expenses.destroy');
+    Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
+    Route::get('/expenses/create', [ExpenseController::class, 'create'])->name('expenses.create');
+    Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+    Route::get('/expenses/{expense}/edit', [ExpenseController::class, 'edit'])->name('expenses.edit');
+    Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
+    Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
  
-    Route::get('/profile',    [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile',  [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/auth/google',            [SocialAuthController::class, 'redirectToGoogle'])->name('auth.google');
+    Route::get('/auth/google/callback',   [SocialAuthController::class, 'handleGoogleCallback'])->name('auth.google.callback');
+    Route::get('/auth/facebook',          [SocialAuthController::class, 'redirectToFacebook'])->name('auth.facebook');
+    Route::get('/auth/facebook/callback', [SocialAuthController::class, 'handleFacebookCallback'])->name('auth.facebook.callback');
 });
  
 require __DIR__.'/auth.php';
